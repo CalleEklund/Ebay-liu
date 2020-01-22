844069497ed2213d8b8c664e48c96334057f8abc
fella149 <fella149@student.liu.se>
2020-01-22 19:04:22 +0100

    han kan funka nu men kolla igenom testfilen en gång till är du snäll

--- PycharmProjects/labbar/app.py
+++ PycharmProjects/labbar/app.py
@@ -1,68 +1,76 @@
 from flask import Flask, jsonify, request
 import uuid
-
+import json
 
 app = Flask(__name__)
 
 
 # global dict
 
-messages = [{'id': '220f7259-beb1-4012-aa59-6e787a0cd581', 'text': 'demo0', 'readBy': []}, {'id': 'a0d84018-d718-4715-a645-ff375d4b3a13', 'text': 'demo1', 'readBy': []}, {'id': 'afe83630-4cbf-4000-baef-38b13145bf65', 'text': 'demo2', 'readBy': []}, {'id': '01d6d904-dc32-45fb-9c36-f6c8719cd690', 'text': 'demo3', 'readBy': []}, {'id': 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb', 'text': 'demo4', 'readBy': []}, {'id': 'ef9c846a-3477-4506-8f5c-a7200d5fab71', 'text': 'demo5', 'readBy': []}, {'id': '2298b7e7-e4e7-48b6-bb13-ea6260d3cfe4', 'text': 'demo6', 'readBy': []}]
-
+messages = {'220f7259-beb1-4012-aa59-6e787a0cd581':{'id': '220f7259-beb1-4012-aa59-6e787a0cd581', 'text': 'demo0', 'readBy': ['a0d84018-d718-4715-a645-ff375d4b3a13']},'a0d84018-d718-4715-a645-ff375d4b3a13':{'id': 'a0d84018-d718-4715-a645-ff375d4b3a13', 'text': 'demo1', 'readBy': []},'afe83630-4cbf-4000-baef-38b13145bf65':{'id': 'afe83630-4cbf-4000-baef-38b13145bf65', 'text': 'demo2', 'readBy': []},'01d6d904-dc32-45fb-9c36-f6c8719cd690':{'id': '01d6d904-dc32-45fb-9c36-f6c8719cd690', 'text': 'demo3', 'readBy': ['a0d84018-d718-4715-a645-ff375d4b3a13']},'a432a82a-6c4b-4c42-b9e0-6713e52b81cb':{'id': 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb', 'text': 'demo4', 'readBy': []},'a432a82a-6c4b-4c42-b9e0-6713e52b81cb': {'id': 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb', 'text': 'demo4', 'readBy': []},'ef9c846a-3477-4506-8f5c-a7200d5fab71':{'id': 'ef9c846a-3477-4506-8f5c-a7200d5fab71', 'text': 'demo5', 'readBy': []},'2298b7e7-e4e7-48b6-bb13-ea6260d3cfe4':{'id': '2298b7e7-e4e7-48b6-bb13-ea6260d3cfe4', 'text': 'demo6', 'readBy': []}}
 
 
 @app.route('/')
 def hello_world():
-    return 'Hellus eVerYbodieS!'
+    return 'hellu erbadies! welcums, to my cribs.'
 
 
 @app.route('/messages/',methods=['POST', 'GET'])
 def message():
+    global messages
     if request.method == 'POST':
-        msg = "demo7"
-        id = uuid.uuid4()
-        data = {'id': id, 'text': msg, 'readBy': []}
-        outdata = {'id': id}
-        messages.append(data)
+        msg = request.json['msg']
+
+        print(msg)
+        uid = uuid.uuid4()
+        data = {'id': uid, 'msg':msg,'readBy': []}
+        messages = {str(uid): data}
+        outdata = {'id':uid}
         return jsonify(outdata)
     elif request.method == 'GET':
-        return jsonify(messages)
+        output = []
+        for post in messages.values():
+            output.append(post)
+        return jsonify(output)
+
+
+@app.route('/messages/<MessageID>',methods=['DELETE','GET'])
+def get_message(MessageID):
+    global messages
+
+    if request.method == 'GET':
+
+        outdata = messages[MessageID]
+        print(outdata)
+        return jsonify(outdata)
+    elif request.method == 'DELETE':
+
+        exists = messages[MessageID]
+        if exists:
+            del messages[MessageID]
+            return "", 200
+        else:
+            return jsonify(""), 200
 
 
 @app.route('/messages/<MessageID>/read/<UserID>', methods=['GET'])
 def mark_read(MessageID, UserID):
-    yete = get_id_msg(MessageID, None)
-    if yete:
-        yete['readBy'].append(str(UserID))
-        return jsonify("")
+    global messages
+    data = messages[str(MessageID)]
+    if data:
+        data['readBy'].append(str(UserID))
+        return "",200
     else:
         return jsonify("")
-    print(messages)
-
-@app.route('/messages/<MessageID>',methods=['DELETE','GET'])
-def get_message(MessageID):
-    id = MessageID
-    print(request.method)
-
-    if request.method == 'GET':
-        out = get_id_msg(id,None)
-        return jsonify(out)
-    elif request.method == 'DELETE':
-
-        elem = get_id_msg(id, None)
-        if elem:
-            messages.remove(elem)
-            return jsonify("test")
-        else:
-            return jsonify("test")
 
 
-def get_id_msg(id=None,msg=None):
-    for elem in messages:
-        if id in elem.values() or msg in elem.values():
-            return elem
-
-    return None
+@app.route('/messages/unread/<UserID>', methods=['GET'])
+def get_unread(UserID):
+    output = []
+    for post in messages.values():
+        if not UserID in post['readBy']:
+            output.append(post)
+    return jsonify(output)
 
 
 if __name__ == '__main__':
--- PycharmProjects/labbar/lab1.py
+++ PycharmProjects/labbar/lab1.py
@@ -1,30 +1,57 @@
 import requests
-import json
 
 
 def save_msg():
-    g = requests.get("http://127.0.0.1:5000/messages") or None
+    #g = requests.get("http://127.0.0.1:5000/messages") or None
+
+    r = requests.post("http://127.0.0.1:5000/messages",json={"msg" : "hello!"}) or None
     """
-    r = requests.post("http://127.0.0.1:5000/messages") or None
-    if check_ok(r) == "ok":
-        data = r.json()
-        return data
+    if check_ok(r) and check_ok(g):
+        data_r = r.json()
+        data_g = g.json()
+        return data_r,data_g
     el
-    """
-    if check_ok(g) == "ok":
+    
+     elif check_ok(g):
         data = g.json()
         return data
+    """
+    if check_ok(r):
+        data = r.json()
+        return data
     else:
-        print(check_ok(g))
+        return check_ok(r)
+
+
+def get_unread():
+    g = requests.get("http://127.0.0.1:5000/messages/unread/a0d84018-d718-4715-a645-ff375d4b3a13")
+    if check_ok(g):
+        print(g.status_code)
+        return g.json()
 
 
 def get_msg():
-    # r = requests.get("http://127.0.0.1:5000/messages/a0d84018-d718-4715-a645-ff375d4b3a13")
-    d = requests.delete("http://127.0.0.1:5000/messages/a0d84018-d718-4715-a645-ff375d4b3a13")
-    if check_ok(d) == "ok":
-        # print(r.json())
-        print(d.status_code)
+    r = requests.get("http://127.0.0.1:5000/messages/676941a1-5c4f-4d62-b9d5-de2b2d46d7e1")
 
+    #d = requests.delete("http://127.0.0.1:5000/messages/a0d84018-d718-4715-a645-ff375d4b3a13")
+    if check_ok(r):
+        data = r.json()
+        return data
+    else:
+        return check_ok(r)
+"""
+    if check_ok(r) and check_ok(d):
+        data_r = r.json()
+        data_d = d.status_code()
+        return data_r,data_d
+    elif check_ok(r):
+        data = r.json()
+        return data
+    elif check_ok(d):
+        return d.status_code
+    else:
+        print(check_ok(d),check_ok(r))
+"""
 
 def check_ok(r):
     if r.status_code == 404:
@@ -36,8 +63,8 @@ def check_ok(r):
     elif r.status_code == 500:
         return "Error"
     else:
-        return "ok"
+        return True
 
 
-# get_msg()
-print(save_msg())
+print(get_msg())
+#print(save_msg())
