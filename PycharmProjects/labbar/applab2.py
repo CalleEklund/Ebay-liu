3beb1340260ea7f7e0a5cc0b6c9f5562435f690e
carek123 <carek123@student.liu.se>
2020-01-27 11:46:49 +0100

    kom lite längre, unique constraint error

--- PycharmProjects/labbar/applab2.py
+++ PycharmProjects/labbar/applab2.py
@@ -7,32 +7,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lab2.db'
 
 db = SQLAlchemy(app)
 
+users_messages = db.Table('users_messages',
+                          db.Column('user_id',db.Integer,db.ForeignKey('user.id'),primary_key=True),
+                          db.Column('message_readBy',db.Integer,db.ForeignKey('message.readBy'),primary_key=True)
+                          )
+
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True,unique=True)
+    __tabelname__ = 'users'
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+
+    messages = db.relationship('Message',secondary=users_messages,back_populates='users')
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.String(36),primary_key=True)
     msg = db.Column(db.String(140),nullable=False)
+    readBy = db.Column(db.Integer,nullable=True)
 
-    def __repr__(self):
-        return '<User %r %l>' % self.id+self.msg
+    users = db.relationship('User',secondary=users_messages,back_populates='messages')
 
-
-def setup():
-    db.create_all()
-    u1 = User(username='test1')
-    u2 = User(username='test2')
-
-    db.session.add(u1)
-    db.session.add(u2)
-    db.session.commit()
+#db.create_all()
 
 def intial_insert():
+
     messages = {'220f7259-beb1-4012-aa59-6e787a0cd581': {'id': '220f7259-beb1-4012-aa59-6e787a0cd581', 'text': 'demo0',
-                                                         'readBy': ['a0d84018-d718-4715-a645-ff375d4b3a13']},
+                                                         'readBy': []},
                 'a0d84018-d718-4715-a645-ff375d4b3a13': {'id': 'a0d84018-d718-4715-a645-ff375d4b3a13', 'text': 'demo1',
                                                          'readBy': []},
                 'afe83630-4cbf-4000-baef-38b13145bf65': {'id': 'afe83630-4cbf-4000-baef-38b13145bf65', 'text': 'demo2',
                                                          'readBy': []},
                 '01d6d904-dc32-45fb-9c36-f6c8719cd690': {'id': '01d6d904-dc32-45fb-9c36-f6c8719cd690', 'text': 'demo3',
-                                                         'readBy': ['a0d84018-d718-4715-a645-ff375d4b3a13']},
+                                                         'readBy': []},
                 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb': {'id': 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb', 'text': 'demo4',
                                                          'readBy': []},
                 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb': {'id': 'a432a82a-6c4b-4c42-b9e0-6713e52b81cb', 'text': 'demo4',
@@ -41,7 +46,12 @@ def intial_insert():
                                                          'readBy': []},
                 '2298b7e7-e4e7-48b6-bb13-ea6260d3cfe4': {'id': '2298b7e7-e4e7-48b6-bb13-ea6260d3cfe4', 'text': 'demo6',
                                                          'readBy': []}}
+    for elem in messages.values():
+        indata = Message(id = elem['id'],msg = elem['text'],readBy=None)
+        db.session.add(indata)
+    db.session.commit()
 
+intial_insert()
 
 def clear_data(session):
     meta = db.metadata
@@ -52,7 +62,7 @@ def clear_data(session):
 #använda för att ta bort överflödig data.
 #clear_data(db.session)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
 
