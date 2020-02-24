cf2345a78d72fd3475f6eb63438031cfd12b4c6f
carek123 <carek123@student.liu.se>
2020-02-24 23:50:40 +0100

    andriod initial commit

--- androidlabbar/app/src/main/java/com/example/labb1android/MainActivity.java
+++ androidlabbar/app/src/main/java/com/example/labb1android/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.labb1android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addText = (Button) findViewById(R.id.addInput);
+
+
+        addText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+                TextView outputText = (TextView) findViewById(R.id.output);
+                String inputStr = (String) input.getText().toString();
+                //testa med att göra en egen append grej, spara undan sen sätt till de nya värdet
+                String oldValue = (String) outputText.getText().toString();
+                String newValue = (String) oldValue + "\nNamn: " + inputStr;
+                //String newValue = Html.fromHtml(oldValue + "&lt;br&gt; Namn: " + inputStr).toString();
+
+                outputText.setText(newValue);
+            }
+        });
     }
 }
