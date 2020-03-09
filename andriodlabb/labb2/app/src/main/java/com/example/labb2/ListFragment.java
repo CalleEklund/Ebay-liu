57c5e16c89221c2103e226588a6efbfae34781c2
Carl Eklund <carek123@student.liu.se>
2020-03-09 18:39:07 +0100

    första fragment

---
+++ andriodlabb/labb2/app/src/main/java/com/example/labb2/ListFragment.java
@@ -0,0 +1,23 @@
+package com.example.labb2;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Lägg in en listview som håller listan samt ett fragment som håller details
+ */
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_list,container,false);
+        return rootView;
+    }
+}
