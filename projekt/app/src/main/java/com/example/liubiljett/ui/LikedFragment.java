706c1ce816f310c16cf1d8229ae645747e188c50
carek123 <carek123@student.liu.se>
2020-04-29 12:50:01 +0200

    inital projekt commit

---
+++ projekt/app/src/main/java/com/example/liubiljett/ui/LikedFragment.java
@@ -0,0 +1,28 @@
+package com.example.liubiljett.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.liubiljett.R;
+
+public class LikedFragment extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_liked, container, false);
+        final TextView textView = root.findViewById(R.id.text_liked);
+        textView.setText("Liked");
+
+        return root;
+    }
+}
