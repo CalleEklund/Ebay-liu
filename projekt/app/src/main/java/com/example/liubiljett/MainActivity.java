d195f1f501cb46d1aaea84b981f545d3a568fda0
felix <fella149@student.liu.se>
2020-07-29 12:18:01 +0200

    testa om det funkar för dig

--- projekt/app/src/main/java/com/example/liubiljett/MainActivity.java
+++ projekt/app/src/main/java/com/example/liubiljett/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.liubiljett;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -9,13 +10,23 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.example.liubiljett.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {  //ANVÄND javac -Xlint:unchecked MainActivity.java
+                                                        //I TERMINALEN OCH GÅ IGENOM FELEN DÄR
+
+    String[] headLineArray = {"Big Chonkus", "ODZ", "Beatles", "LasseMajas detektivbyrå"};
+    String[] priceArray = {"100kr", "120kr", "999kr", "100kr"};
+    Integer[] imageArray = {R.drawable.account, R.drawable.comment, R.drawable.heart, R.drawable.search};
+
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        MyAdapter mAdapter = new MyAdapter(this, headLineArray, priceArray, imageArray);
+        listView = findViewById(R.id.listviewID);
+        listView.setAdapter(mAdapter);
+
+        //github.com/crazycodeboy/react-native-splash-screen/issues/
     }
 
 }
