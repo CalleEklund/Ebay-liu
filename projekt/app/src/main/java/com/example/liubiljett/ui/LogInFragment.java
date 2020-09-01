6ca35c992807672fc03b9055cf8fc50f32d2b6c6
felix <fella149@student.liu.se>
2020-09-01 15:33:27 +0200

    login och transaction mellan login och create account funkar

--- projekt/app/src/main/java/com/example/liubiljett/ui/LogInFragment.java
+++ projekt/app/src/main/java/com/example/liubiljett/ui/LogInFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,15 +18,20 @@ public class LogInFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        final View root = inflater.inflate(R.layout.fragment_login, container, false);
+
         final TextView textView = root.findViewById(R.id.register_link);
         textView.setOnClickListener(new AdapterView.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.relative_layout, new ProfileFragment());
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_login, new ProfileFragment()).commit();
+                Button loginButton = root.findViewById(R.id.login_button);
+                loginButton.setVisibility(View.INVISIBLE);
 
             }
         });
+
+
 
         return root;
     }
